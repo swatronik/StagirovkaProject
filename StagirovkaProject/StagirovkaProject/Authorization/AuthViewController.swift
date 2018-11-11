dc070b5d29514dad4ebbe62741b3b34513fe337e
Denis Polyakov <swatronik@gmail.com>
2018-11-11 20:39:45 +0300

    Auth_2

--- StagirovkaProject/StagirovkaProject/Authorization/AuthViewController.swift
+++ StagirovkaProject/StagirovkaProject/Authorization/AuthViewController.swift
@@ -8,13 +8,34 @@
 
 import UIKit
 
-class AuthViewController: UIViewController {
+@IBDesignable class AuthViewController: UIViewController {
     
-    @IBOutlet weak var contentView: AuthView!
+    @IBOutlet weak var tableView: UITableView!
+    //@IBOutlet weak var contentView: AuthView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    let authCellId = "AuthTableViewCell"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setGradientBackground()
+        tableView.register(UINib.init(nibName: authCellId, bundle: nil), forCellReuseIdentifier: authCellId)
+        tableView.allowsSelection = false
+        tableView.layer.cornerRadius = 15
+        //scrollView?.keyboardDismissMode = .interactive
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    @objc func keyboard(notification: Notification) {
+        let userInfo = notification.userInfo!
+        
+        let keyboardScreenEndFrame: CGRect = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            tableView.contentInset = UIEdgeInsets.zero
+        } else {
+            tableView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height - (view.bounds.height/2 - 150), right: 0)
+        }
     }
     
     func setGradientBackground() {
@@ -28,4 +49,21 @@ class AuthViewController: UIViewController {
         self.view.layer.insertSublayer(gradientLayer, at: 0)
     }
 
+}
+
+
+extension AuthViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: authCellId, for: indexPath) as! AuthTableViewCell
+        return cell
+    } 
 }
